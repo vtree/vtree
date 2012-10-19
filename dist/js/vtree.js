4a909e8e054fd64c52d3cd3e19bf3231fa7eb1fe
Loic <lginoux@gmail.com>
2012-10-19 11:11:58 +0200

    removed broken xml source plugin

--- dist/js/vtree.js
+++ dist/js/vtree.js
@@ -1334,307 +1334,194 @@ Vtree.plugins.defaults.core.node = {
 })(jQuery);(function($) {
 
 
-/**
- * jQuery client-side XSLT plugins.
- *
- * @author <a href="mailto:jb@eaio.com">Johann Burkard</a>
- * @version $Id: jquery.xslt.js,v 1.10 2008/08/29 21:34:24 Johann Exp $
- */
-/*
- * xslt.js
- *
- * Copyright (c) 2005-2008 Johann Burkard (<mailto:jb@eaio.com>)
- * <http://eaio.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
- * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
+/*	This work is licensed under Creative Commons GNU LGPL License.
 
-/**
- * Constructor for client-side XSLT transformations.
- *
- * @author <a href="mailto:jb@eaio.com">Johann Burkard</a>
- * @version $Id: xslt.js,v 1.7 2008/08/29 21:22:55 Johann Exp $
- * @constructor
- */
-	var Transformation = function() {
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 0.9
+	Author:  Stefan Goessner/2006
+	Web:     http://goessner.net/
+*/
+function xml2json(xml, tab) {
+   var X = {
+      toObj: function(xml) {
+         var o = {};
+         if (xml.nodeType==1) {   // element node ..
+            if (xml.attributes.length)   // element with attributes  ..
+               for (var i=0; i<xml.attributes.length; i++)
+                  o["@"+xml.attributes[i].nodeName] = (xml.attributes[i].nodeValue||"").toString();
+            if (xml.firstChild) { // element has child nodes ..
+               var textChild=0, cdataChild=0, hasElementChild=false;
+               for (var n=xml.firstChild; n; n=n.nextSibling) {
+                  if (n.nodeType==1) hasElementChild = true;
+                  else if (n.nodeType==3 && n.nodeValue.match(/[^ \f\n\r\t\v]/)) textChild++; // non-whitespace text
+                  else if (n.nodeType==4) cdataChild++; // cdata section node
+               }
+               if (hasElementChild) {
+                  if (textChild < 2 && cdataChild < 2) { // structured element with evtl. a single text or/and cdata node ..
+                     X.removeWhite(xml);
+                     for (var n=xml.firstChild; n; n=n.nextSibling) {
+                        if (n.nodeType == 3)  // text node
+                           o["#text"] = X.escape(n.nodeValue);
+                        else if (n.nodeType == 4)  // cdata node
+                           o["#cdata"] = X.escape(n.nodeValue);
+                        else if (o[n.nodeName]) {  // multiple occurence of element ..
+                           if (o[n.nodeName] instanceof Array)
+                              o[n.nodeName][o[n.nodeName].length] = X.toObj(n);
+                           else
+                              o[n.nodeName] = [o[n.nodeName], X.toObj(n)];
+                        }
+                        else  // first occurence of element..
+                           o[n.nodeName] = X.toObj(n);
+                     }
+                  }
+                  else { // mixed content
+                     if (!xml.attributes.length)
+                        o = X.escape(X.innerXml(xml));
+                     else
+                        o["#text"] = X.escape(X.innerXml(xml));
+                  }
+               }
+               else if (textChild) { // pure text
+                  if (!xml.attributes.length)
+                     o = X.escape(X.innerXml(xml));
+                  else
+                     o["#text"] = X.escape(X.innerXml(xml));
+               }
+               else if (cdataChild) { // cdata
+                  if (cdataChild > 1)
+                     o = X.escape(X.innerXml(xml));
+                  else
+                     for (var n=xml.firstChild; n; n=n.nextSibling)
+                        o["#cdata"] = X.escape(n.nodeValue);
+               }
+            }
+            if (!xml.attributes.length && !xml.firstChild) o = null;
+         }
+         else if (xml.nodeType==9) { // document.node
+            o = X.toObj(xml.documentElement);
+         }
+         else
+            alert("unhandled node type: " + xml.nodeType);
+         return o;
+      },
+      toJson: function(o, name, ind) {
+         var json = name ? ("\""+name+"\"") : "";
+         if (o instanceof Array) {
+            for (var i=0,n=o.length; i<n; i++)
+               o[i] = X.toJson(o[i], "", ind+"\t");
+            json += (name?":[":"[") + (o.length > 1 ? ("\n"+ind+"\t"+o.join(",\n"+ind+"\t")+"\n"+ind) : o.join("")) + "]";
+         }
+         else if (o == null)
+            json += (name&&":") + "null";
+         else if (typeof(o) == "object") {
+            var arr = [];
+            for (var m in o)
+               arr[arr.length] = X.toJson(o[m], m, ind+"\t");
+            json += (name?":{":"{") + (arr.length > 1 ? ("\n"+ind+"\t"+arr.join(",\n"+ind+"\t")+"\n"+ind) : arr.join("")) + "}";
+         }
+         else if (typeof(o) == "string")
+            json += (name&&":") + "\"" + o.toString() + "\"";
+         else
+            json += (name&&":") + o.toString();
+         return json;
+      },
+      innerXml: function(node) {
+         var s = ""
+         if ("innerHTML" in node)
+            s = node.innerHTML;
+         else {
+            var asXml = function(n) {
+               var s = "";
+               if (n.nodeType == 1) {
+                  s += "<" + n.nodeName;
+                  for (var i=0; i<n.attributes.length;i++)
+                     s += " " + n.attributes[i].nodeName + "=\"" + (n.attributes[i].nodeValue||"").toString() + "\"";
+                  if (n.firstChild) {
+                     s += ">";
+                     for (var c=n.firstChild; c; c=c.nextSibling)
+                        s += asXml(c);
+                     s += "</"+n.nodeName+">";
+                  }
+                  else
+                     s += "/>";
+               }
+               else if (n.nodeType == 3)
+                  s += n.nodeValue;
+               else if (n.nodeType == 4)
+                  s += "<![CDATA[" + n.nodeValue + "]]>";
+               return s;
+            };
+            for (var c=node.firstChild; c; c=c.nextSibling)
+               s += asXml(c);
+         }
+         return s;
+      },
+      escape: function(txt) {
+         return txt.replace(/[\\]/g, "\\\\")
+                   .replace(/[\"]/g, '\\"')
+                   .replace(/[\n]/g, '\\n')
+                   .replace(/[\r]/g, '\\r');
+      },
+      removeWhite: function(e) {
+         e.normalize();
+         for (var n = e.firstChild; n; ) {
+            if (n.nodeType == 3) {  // text node
+               if (!n.nodeValue.match(/[^ \f\n\r\t\v]/)) { // pure whitespace text node
+                  var nxt = n.nextSibling;
+                  e.removeChild(n);
+                  n = nxt;
+               }
+               else
+                  n = n.nextSibling;
+            }
+            else if (n.nodeType == 1) {  // element node
+               X.removeWhite(n);
+               n = n.nextSibling;
+            }
+            else                      // any other node
+               n = n.nextSibling;
+         }
+         return e;
+      }
+   };
+   if (xml.nodeType == 9) // document node
+      xml = xml.documentElement;
+   var json = X.toJson(X.toObj(X.removeWhite(xml)), xml.nodeName, "\t");
+   return "{\n" + tab + (tab ? json.replace(/\t/g, tab) : json.replace(/\t|\n/g, "")) + "\n}";
+}
 
-		var xml;
+function parseXml(xml) {
+   var dom = null;
+   if (window.DOMParser) {
+      try {
+         dom = (new DOMParser()).parseFromString(xml, "text/xml");
+      }
+      catch (e) { dom = null; }
+   }
+   else if (window.ActiveXObject) {
+      try {
+         dom = new ActiveXObject('Microsoft.XMLDOM');
+         dom.async = false;
+         if (!dom.loadXML(xml)) // parse error ..
 
-		var xmlDoc;
-
-		var xslt;
-
-		var xsltDoc;
-
-		var callback = function() {};
-
-		/**
-		 * Sort of like a fix for Opera who doesn't always get readyStates right.
-		 */
-		var transformed = false;
-
-		/**
-		 * Returns the URL of the XML document.
-		 *
-		 * @return the URL of the XML document
-		 * @type String
-		 */
-		this.getXml = function() {
-			return xml;
-		};
-
-		/**
-		 * Returns the XML document.
-		 *
-		 * @return the XML document
-		 */
-		this.getXmlDocument = function() {
-			return xmlDoc;
-		};
-
-		/**
-		 * Sets the URL of the XML document.
-		 *
-		 * @param x the URL of the XML document
-		 * @return this
-		 * @type Transformation
-		 */
-		this.setXml = function(x) {
-			xml = x;
-			return this;
-		};
-
-		/**
-		 * Returns the URL of the XSLT document.
-		 *
-		 * @return the URL of the XSLT document
-		 * @type String
-		 */
-		this.getXslt = function() {
-			return xslt;
-		};
-
-		/**
-		 * Returns the XSLT document.
-		 *
-		 * @return the XSLT document
-		 */
-		this.getXsltDocument = function() {
-			return xsltDoc;
-		};
-
-		/**
-		 * Sets the URL of the XSLT document.
-		 *
-		 * @param x the URL of the XML document
-		 * @return this
-		 * @type Transformation
-		 */
-		this.setXslt = function(x) {
-			xslt = x;
-			return this;
-		};
-
-		/**
-		 * Returns the callback function.
-		 *
-		 * @return the callback function
-		 */
-		this.getCallback = function() {
-			return callback;
-		};
-
-		/**
-		 * Sets the callback function
-		 *
-		 * @param c the callback function
-		 * @return this
-		 * @type Transformation
-		 */
-		this.setCallback = function(c) {
-			callback = c;
-			return this;
-		};
-
-		/**
-		 *
-		 * This method may only be called after {@link #setXml} and {@link #setXslt} have
-		 * been called.
-		 * <p>
-		 *
-		 * @return the result of the transformation
-		 */
-		this.transform = function() {
-			if(!browserSupportsXSLT()) {
-				return;
-			}
-			var str = /^\s*</;
-			var t = this,
-				res,
-				change,
-				xm,
-				xs;
-			if(document.recalc) {
-				change = function() {
-						var c = 'complete';
-						if(xm.readyState == c && xs.readyState == c) {
-							window.setTimeout(function() {
-								xmlDoc = xm.XMLDocument;
-								xsltDoc = xs.XMLDocument;
-								callback(t);
-								res = xm.transformNode(xs.XMLDocument);
-							}, 50);
-						}
-					};
-
-				xm = document.createElement('xml');
-				xm.onreadystatechange = change;
-				xm[str.test(xml) ? "innerHTML" : "src"] = xml;
-
-				xs = document.createElement('xml');
-				xs.onreadystatechange = change;
-				xs[str.test(xslt) ? "innerHTML" : "src"] = xslt;
-
-				document.body.insertBefore(xm);
-				document.body.insertBefore(xs);
-
-			} else {
-				var transformed = false;
-
-				xm = {
-					readyState: 4
-				};
-				xs = {
-					readyState: 4
-				};
-				change = function() {
-						if(xm.readyState == 4 && xs.readyState == 4 && !transformed) {
-							xmlDoc = xm.responseXML;
-							xsltDoc = xs.responseXML;
-							var resultDoc;
-							var processor = new XSLTProcessor();
-
-							if(typeof processor.transformDocument == 'function') {
-								// obsolete Mozilla interface
-								resultDoc = document.implementation.createDocument("", "", null);
-								processor.transformDocument(xm.responseXML, xs.responseXML, resultDoc, null);
-								var out = new XMLSerializer().serializeToString(resultDoc);
-								callback(t);
-								res = out;
-							} else {
-								processor.importStylesheet(xs.responseXML);
-								resultDoc = processor.transformToFragment(xm.responseXML, document);
-								callback(t);
-								res = resultDoc;
-							}
-
-							transformed = true;
-						}
-					};
-
-				if(str.test(xml)) {
-					xm.responseXML = new DOMParser().parseFromString(xml, "text/xml");
-				} else {
-					xm = new XMLHttpRequest();
-					xm.onreadystatechange = change;
-					xm.open("GET", xml);
-					xm.send(null);
-				}
-
-				if(str.test(xslt)) {
-					xs.responseXML = new DOMParser().parseFromString(xslt, "text/xml");
-					change();
-				} else {
-					xs = new XMLHttpRequest();
-					xs.onreadystatechange = change;
-					xs.open("GET", xslt);
-					xs.send(null);
-				}
-			}
-			return res;
-		};
-
-	};
-
-	/**
-	 * Returns whether the browser supports XSLT.
-	 *
-	 * @return the browser supports XSLT
-	 * @type boolean
-	 */
-	var browserSupportsXSLT = function() {
-		var support = false;
-		if(document.recalc) { // IE 5+
-			support = true;
-		} else if(window.XMLHttpRequest !== undefined && window.XSLTProcessor !== undefined) { // Mozilla 0.9.4+, Opera 9+
-			var processor = new XSLTProcessor();
-			if(typeof processor.transformDocument == 'function') {
-				support = window.XMLSerializer !== undefined;
-			} else {
-				support = true;
-			}
-		}
-		return support;
-	};
-
+            window.alert(dom.parseError.reason + dom.parseError.srcText);
+      }
+      catch (e) { dom = null; }
+   }
+   else
+      alert("cannot parse xml string!");
+   return dom;
+}
 
 	Vtree.plugins.xmlSource = {
 		nodeStore: {
-			defaults: {
-				xslt: '' +
-				'<?xml version="1.0" encoding="utf-8" ?>' +
-				'<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" >' +
-				'<xsl:output encoding="utf-8"  indent="no" omit-xml-declaration="yes" method="text" />' +
-				'' +
-				'<xsl:strip-space elements="*" />' +
-				'<xsl:template match="/">' +
-				'	<xsl:apply-templates select="tree"/>' +
-				'</xsl:template>' +
-				'' +
-				'<xsl:template match="tree">' +
-				'	{"tree":{' +
-				'		"id": "<xsl:value-of select="@id"/>",' +
-				'	"nodes": <xsl:apply-templates select="nodes"/> }' +
-				'	}' +
-				'</xsl:template>' +
-				'' +
-				'<xsl:template match="nodes">' +
-				' [<xsl:apply-templates select="node"/>]' +
-				'</xsl:template>' +
-				'' +
-				'<xsl:template match="node">' +
-				'	{"id": "<xsl:value-of select="id"/>",' +
-				'	"title": "<xsl:value-of select="label|title"/>",' +
-				'	"description": "<xsl:value-of select="description"/>",' +
-				'	"hasChildren": <xsl:value-of select="hasChildren"/>,' +
-				'	"iconPath": "<xsl:value-of select="icon"/>",' +
-				'	"nodes":[<xsl:apply-templates select="node"/>]' +
-				'	}<xsl:if test="position()!=last()">,</xsl:if>' +
-				'</xsl:template>' +
-				'</xsl:stylesheet>'
-			},
 			_fn: {
 				getDataSource: function(attribute) {
-					var res = new Transformation().setXml(this.tree.dataSource).setXslt(this.xslt).transform();
-
-					this.tree.dataSource = JSON.parse(res.textContent);
+					//var res = new Transformation().setXml(this.tree.dataSource).setXslt(this.xslt).transform();
+					var dom = parseXml(this.tree.dataSource);
+    				var json = xml2json(dom, "");
+    				console.log(json)
+					this.tree.dataSource = JSON.parse(json);
 					return this._call_prev();
 				}
 			}
